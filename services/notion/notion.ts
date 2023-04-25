4e0f82f34bab45c8f93fdaa5dca35b5ae218cae4
twosaturdayscode <ali.sbtan96@gmail.com>
2023-04-25 22:08:21 +0200

    (kv): fix commits mess

--- services/notion/notion.ts
+++ services/notion/notion.ts
@@ -5,6 +5,7 @@ import type {
   BlogService,
   GetPublishedPostsOptions,
 } from '@/interfaces/blog'
+import { BlogPostSchema } from '@/interfaces/blog'
 import { Client as NotionClient, isFullPage } from '@notionhq/client'
 import { NotionToMarkdown } from 'notion-to-md'
 import type { ValidProperties } from '@/notion/validation'
@@ -33,6 +34,11 @@ export class NotionService implements BlogService {
   }
 
   async getPublishedPosts(options?: GetPublishedPostsOptions) {
+    const cachedResults = await this.kv.get('blog:all-posts', 'json')
+
+    if (cachedResults != null)
+      return BlogPostSchema.array().parse(cachedResults)
+
     const { results } = await this.notion.databases.query({
       database_id: this.database_id,
       filter: {
@@ -50,33 +56,31 @@ export class NotionService implements BlogService {
       page_size: options?.size !== 'all' ? options?.size : undefined,
     })
 
-    const posts: BlogPost[] = await Promise.all(
-      results
-        .filter((r): r is PageObjectResponse => isFullPage(r))
-        .filter((p): p is typeof p & { properties: ValidProperties } =>
-          validatePageProperties(p.properties).isOk()
-        )
-        .map(async p => {
-          const validProps = { ...p.properties }
+    const posts: BlogPost[] = results
+      .filter((r): r is PageObjectResponse => isFullPage(r))
+      .filter((p): p is typeof p & { properties: ValidProperties } =>
+        validatePageProperties(p.properties).isOk()
+      )
+      .map(p => {
+        const validProps = { ...p.properties }
 
-          const imageRes = await fetch(validProps.image.files[0].file.url)
-          const image = await imageRes.text()
-          const imageBase64 = btoa(image)
+        return {
+          id: p.id,
+          title: validProps.title.rich_text.map(r => r.plain_text).join(' '),
+          description: validProps.description.rich_text
+            .map(r => r.plain_text)
+            .join(' '),
+          status: 'published',
+          tags: validProps.tags.multi_select.map(m => m.name),
+          slug: validProps.slug.formula.string,
+          image: validProps.image.files[0].file.url,
+          publishedAt: new Date(validProps['published-at'].date.start),
+        }
+      })
 
-          return {
-            id: p.id,
-            title: validProps.title.rich_text.map(r => r.plain_text).join(' '),
-            description: validProps.description.rich_text
-              .map(r => r.plain_text)
-              .join(' '),
-            status: 'published',
-            tags: validProps.tags.multi_select.map(m => m.name),
-            slug: validProps.slug.formula.string,
-            image: `data:image/svg+xml;base64,${imageBase64}`,
-            publishedAt: new Date(validProps['published-at'].date.start),
-          }
-        })
-    )
+    this.kv.put('blog:all-posts', JSON.stringify(posts), {
+      expirationTtl: 60 * 60 * 24,
+    })
 
     return posts
   }
